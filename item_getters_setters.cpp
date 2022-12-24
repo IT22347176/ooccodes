efa8045dda60192d6257706ebcee8575d112dcbc
Dulmina Peiris <121035932+dulminas@users.noreply.github.com>
2022-12-24 17:56:35 +0530

    Update item_getters_setters.cpp

--- item_getters_setters.cpp
+++ item_getters_setters.cpp
@@ -1,58 +1,66 @@
 #include <iostream>
-#include <iomanip>
 #include <cstring>
-using namespace std; 
+using namespace std;
 
-class Item{
-	private:
+class Item {
+	private :
 		int itemCode;
 		char name[20];
 		float price;
-	public:
-		void setItemDetails(int no, char iname); 
-		void setprice(float iprice);
+	public: 
+		void setItemDetails(int no, char pName[]);
+		void setPrice(float pPrice);
 		int getItemCode();
-		float getPrice();  
+		float getPrice();
 };
 
-void Item::setItemDetails(int no,char iname){
-	itemCode=no;
-	strcpy(name,iname);
-}
-
-void Item::setprice(float iprice){
-	price=iprice; 
-}
-
-int Item::getItemCode(){
-	return itemCode;
-}
-
-float Item::getPrice(){
-	return price; 
-}
-
-
-int main(void){
-	Item item1; 
+int main() {
+	
+	Item itm1;
 	int i_code;
 	char i_name[20];
 	float i_price;
 	
-	cout<<"Input item code: ";
-	cin>>i_code;
-	cout<<"Input item name: "; 
-	cin>>i_name; 
-	cout<<"Input item price: "; 
-	cin>>i_price; 
+	cout << "Input Item code : ";
+	cin >> i_code;
+	cout << "Input Item Name : ";
+	cin >> i_name;
+	cout << "Input Item price : ";
+	cin >> i_price;
 	
-	item1.setItemDetails(i_code,i_name); 
-	item1.setprice(i_price); 
+	itm1.setItemDetails(i_code, i_name);
+	itm1.setPrice(i_price);
 	
-	cout<<"Item code: "<<item1.getItemCode()<<endl;
-	cout<<"Item price: "<<item1.getPrice()<<endl; 
+	cout << "Item Code : " << itm1.getItemCode() << endl;
+	cout << "Item price : " << itm1.getPrice() << endl;
 	
-	
-	
+	return 0;
 }
 
+void Item::setItemDetails(int no, char pName[]) 
+{
+	itemCode = no;
+	strcpy(name, pName);
+}
+	
+void Item::setPrice(float pPrice)
+{
+	price= pPrice;
+}
+
+int Item::getItemCode() 
+{
+	return itemCode;
+}
+
+float Item::getPrice()
+{
+	return price;
+}
+
+
+
+	
+	
+	
+	
